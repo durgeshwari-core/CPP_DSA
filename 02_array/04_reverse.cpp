7e329f2bb7814b036bea4b2ee80d03e1467a2812
Durgeshwari sahu <sheryasahu505@gmail.com>
2026-06-04 12:53:32 +0530

    reverse

---
+++ 02_array/04_reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void reverse(int arr[],int n){
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i];
+    }
+}
+int main(){
+    int n ,arr[100];
+    cin>>n;
+    cout<<"enter element ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    reverse(arr,n);
+}
